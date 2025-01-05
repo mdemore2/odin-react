46ecb2584b3ffcc64e1fe18eaa084483f5b06d1f
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-01-05 15:52:55 -0500

    wip: working through lessons

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,12 +1,29 @@
+function ListItem(props) {
+    return <li>{props.item}</li>
+}
+
+function List(props) {
+    if (!props.items) {
+        return <div>Loading...</div>;
+    }
+    if (props.items.length === 0){
+        return <div>There is nothing in the list!</div>;
+    }
+    return <ul>
+        {props.items.map((item) => {
+            return item.includes("y") ? <ListItem key={item} item={item}/> : null;
+        })}
+    </ul>
+
+}
+
 function Movies() {
     const movies = ["Dirty Harry", "The Outlaw Josey Wales", "Heartbreak Ridge", "Million Dollar Baby"];
+    //const movies = null;
+    //const movies = [];
     return <div>
         <h1>Movies: </h1>
-        <ul>
-            {movies.map((movie) => {
-                return <li key={movie}>{movie}</li>
-            })}
-        </ul>
+        <List items={movies} />
     </div>;
 }
 
