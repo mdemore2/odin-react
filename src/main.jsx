7583e92f1a4bd297b18bd013558bd18f5775f835
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-01-05 20:54:18 -0500

    wip: working through lessons

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 //import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import Movies from './Movies.jsx'
+import Colors from './Colors.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
     <Movies />
+    <Colors />
   </StrictMode>,
 )
