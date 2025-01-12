44e1b846c9900b2682f5fca5745504886b122adc
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-01-12 17:51:26 -0500

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-//import App from './App.jsx'
-import Greeting from './Greeting.jsx'
-import Movies from './Movies.jsx'
-import Colors from './Colors.jsx'
-import Person from './Person.jsx'
-import ClassInput from './ClassInput.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./components/Profile";
+import ErrorPage from "./components/ErrorPage";
+import routes from "./routes";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClassInput />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
